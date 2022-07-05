3865cc397e1d750278b5278f1f2c843b08e9538a
Watai210 <106651735+Watai210@users.noreply.github.com>
2022-07-05 09:00:56 +0200

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function gettxt() {
+    var text = document.getElementById("txtbox").value;
+    alert(text);
+}
+
+function settxt() {
+    var text = document.getElementById("txtbox").value = 'Hello World !';
+
+}
