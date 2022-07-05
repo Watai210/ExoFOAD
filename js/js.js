71d171c3bee6181c903dd03a921175707683fa1e
Watai210 <106651735+Watai210@users.noreply.github.com>
2022-07-05 10:57:23 +0200

    .

--- js/js.js
+++ js/js.js
@@ -1,10 +1,45 @@
-function gettxt() {
-    var text = document.getElementById("txtbox").value;
-    alert(text);
+// function gettxt() {
+//     var text = document.getElementById("txtbox").value;
+//     alert(text);
+// }
+
+// function settxt() {
+//     var text = document.getElementById("txtbox").value;
+//     alert('Hello World !');
+
+// }
+function showHxContent1() {
+    var content = document.getElementById('title1');
+    
+    alert('test')
+    if (content.style.display === 'none'){
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
 }
 
-function settxt() {
-    var text = document.getElementById("txtbox").value;
-    alert('Hello World !');
-
+function showHxContent2() {
+    var content = document.getElementById('title2');
+    
+    alert('test2')
+    if (content.style.display === 'none'){
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
 }
+
+// function createEventHandler(items, i) {
+    
+//     return function() {
+//       alert(items[i].querySelector('.title').innerHTML);
+//     };
+//   }
+//   onload = function() {
+//     var show = document.getElementById('bod');
+//     var items = show.getElementsByTagName('p');
+//     for (var i = 0; i < items.length; i++) {
+//       items[i].onclick = createEventHandler(items, i);
+//     }
+//   };
